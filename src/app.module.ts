8d0d8aee30d71b3b19f5030af63f9bf218e78399
swiftintellij <swiftintellij@gmail.com>
2020-04-29 09:42:11 +0900

    make it better

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import Next from 'next';
+import { RenderModule } from 'nest-next';
+
+@Module({
+  imports: [
+    RenderModule.forRootAsync(Next({ dev: process.env.NODE_ENV !== 'production' })),    
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
